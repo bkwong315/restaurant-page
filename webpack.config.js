9e2ed1d52cc6883ebaff9251ea718b344eae7860
bkwong315 <50161878+bkwong315@users.noreply.github.com>
2023-01-11 18:12:54 -0800

    Add webpack-dev-server and npm scripts

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,9 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
   },
   devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
   plugins: [new ESLintPlugin()],
   module: {
     rules: [
