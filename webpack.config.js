fd853500bb13c6c133a9c03f9471d5aed76f0c83
bkwong315 <50161878+bkwong315@users.noreply.github.com>
2023-01-11 18:18:55 -0800

    Update webpack config to clean dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
   devtool: 'inline-source-map',
   devServer: {
@@ -23,7 +24,6 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
-      {},
     ],
   },
 };
