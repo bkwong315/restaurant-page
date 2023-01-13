ce6895a9f6e219b7a465e80892c0da090dc3dc4b
bkwong315 <50161878+bkwong315@users.noreply.github.com>
2023-01-12 17:26:11 -0800

    Update blur div to a pseudo element

--- src/index.js
+++ src/index.js
@@ -2,12 +2,8 @@ import './styles/style.scss';
 
 const App = () => {
   let appContainer = document.createElement('div');
-  let bgBlur = document.createElement('div');
 
   appContainer.classList.add('app');
-  bgBlur.classList.add('bg-blur');
-
-  appContainer.appendChild(bgBlur);
 
   return appContainer;
 };
