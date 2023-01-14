8969aaa8278348a0a4079ba9892b3615353d8532
bkwong315 <50161878+bkwong315@users.noreply.github.com>
2023-01-13 20:38:58 -0800

    Add content container to app container

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import Navbar from './components/Navbar/Navbar';
 import HomePage from './components/HomePage/HomePage';
+import ContactPage from './components/ContactPage/ContactPage';
 
 import './styles/style.scss';
 
 const App = (() => {
   let appContainer = document.createElement('div');
+  let contentContainer = document.createElement('div');
 
   appContainer.classList.add('app');
+  contentContainer.classList.add('content-container');
 
   appContainer.appendChild(Navbar);
-  appContainer.appendChild(HomePage);
+  appContainer.appendChild(contentContainer);
+
+  contentContainer.appendChild(HomePage);
+  contentContainer.appendChild(ContactPage);
 
   return appContainer;
 })();
