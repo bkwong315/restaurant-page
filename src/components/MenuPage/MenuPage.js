b716427b48a887da7f55b523827043564d8b49a5
bkwong315 <50161878+bkwong315@users.noreply.github.com>
2023-01-14 17:18:25 -0800

    Add grilled salmon menu item

--- src/components/MenuPage/MenuPage.js
+++ src/components/MenuPage/MenuPage.js
@@ -1,6 +1,7 @@
 import './MenuPage.scss';
 import vegOmeletImg from './imgs/veg-omelet.jpg';
 import clubSandwichImg from './imgs/club-sandwich.jpg';
+import grilledSalmonImg from './imgs/grilled-salmon.jpg';
 
 import MenuItem from './MenuItem';
 
@@ -13,11 +14,19 @@ const MenuPage = (() => {
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
     '$8.99'
   );
+
   const clubSandwich = MenuItem(
     'Club Sandwich',
     clubSandwichImg,
     'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     '$9.99'
+  );
+
+  const grilledSalmon = MenuItem(
+    'Grilled Salmon',
+    grilledSalmonImg,
+    'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    '$14.99'
   );
 
   heading.textContent = 'Menu';
@@ -27,6 +36,7 @@ const MenuPage = (() => {
   MenuPage.appendChild(heading);
   MenuPage.appendChild(vegOmelet);
   MenuPage.appendChild(clubSandwich);
+  MenuPage.appendChild(grilledSalmon);
 
   return MenuPage;
 })();
